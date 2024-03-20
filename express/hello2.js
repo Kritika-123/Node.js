43fcf4a5fd5efad1e7e390347890f4a766244af4
Kritika <Kritika@12kritika77.com>
2024-03-20 14:04:03 +0530

    hello

--- express/hello2.js
+++ express/hello2.js
@@ -3,9 +3,17 @@ var app=express();
 app.get('/get',(req,res)=>
 {
     res.sendFile(__dirname+"/"+"a.html")
-})
+});
 app.get('/formload',(req,res)=>
 {
     res.sendFile(__dirname+"/"+"formnew.html")
-})
+});
+app.get('/submit',(req,res)=>
+{
+    response={
+name:req.query.name,
+age:req.query.age
+    }
+    res.send(JSON.stringify(response));
+});
 app.listen(2000);
