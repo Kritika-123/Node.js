36fecbb222160ed95b96961ab66f65614fd3ed94
Kritika <Kritika@12kritika77.com>
2024-04-01 09:23:24 +0530

    hello

--- express/hello2.js
+++ express/hello2.js
@@ -1,3 +1,24 @@
+// var express=require('express');
+// var app=express();
+// app.get('/get',(req,res)=>
+// {
+//     res.sendFile(__dirname+"/"+"a.html")
+// });
+// app.get('/formload',(req,res)=>
+// {
+//     res.sendFile(__dirname+"/"+"formnew.html")
+// });
+// app.get('/submit',(req,res)=>
+// {
+//     response={
+// name:req.query.name,
+// age:req.query.age
+//     }
+//     res.send(JSON.stringify(response));
+// });
+// app.listen(2000);
+
+
 var express=require('express');
 var app=express();
 app.get('/get',(req,res)=>
@@ -14,6 +35,9 @@ app.get('/submit',(req,res)=>
 name:req.query.name,
 age:req.query.age
     }
-    res.send(JSON.stringify(response));
+   res.write('<table border="2">');
+   res.write('<tr><th>Name</th><th>Age</th><tr>');
+   res.write('<tr>');
 });
 app.listen(2000);
+
