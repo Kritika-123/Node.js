4524648702e7cc78f679fceed6035655c5b43e91
Kritika <Kritika@12kritika77.com>
2024-04-03 14:32:52 +0530

    hello

--- express/socket3.js
+++ express/socket3.js
@@ -1,17 +1,22 @@
 var express = require('express');
-var app = express(); b
+var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 app.get('/', function(req, res) {
-res.sendFile( __dirname + "/" + "socket.html" );
- //res.send('Hello I am in Socket.io')
+res.sendFile( __dirname + "/" + "socket3.html" );
 });
 io.on('connection', function(socket) {
 console.log('A user connected');
+let counter = 0;
+//Send a message when
+setInterval(function() {
+//Sending an object when emmiting an event
+socket.emit('event', counter++)
+}, 4000);
 socket.on('disconnect', function () {
 console.log('A user disconnected');
 });
 });
 http.listen(2000, function() {
 console.log('listening on localhost:2000');
-})
+});
